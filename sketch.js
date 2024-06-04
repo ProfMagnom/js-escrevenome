0e155621873620556fbed5dd69623346f29bdcf0
ProfMagnom <169149871+ProfMagnom@users.noreply.github.com>
2024-06-04 08:02:36 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,13 @@
+//Interestelar, 10, aventura, ficção científica
+//Efeito Borboleta, 14, ficção científica, drama
+//Dark, 16, ficção científica, mistério
+//Bates Motel, 16, drama, suspense
+//The Umbrella Academy, 16, aventura, comédia
+
+function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background(220);
+  }
